630b61eec123e81432ee7808300eb65ca41abf6d
m4ttia <mattia.garavaglia1@studenti.unimi.it>
2022-10-21 15:47:05 +0200

    Creazione modello strutturato per un utente

---
+++ assets/models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  cities: [{
+    name:{
+      type: String,
+      required: true
+    },
+    country:{
+      type: String
+    }
+  }]
+}, {timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
