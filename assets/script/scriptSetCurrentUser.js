e71347ccacdaead9a1447ddb8c56a8b7f39f9da3
m4ttia <mattia.garavaglia1@studenti.unimi.it>
2022-11-15 10:49:43 +0100

    Creazione script per settare l'utente corrente

---
+++ assets/script/scriptSetCurrentUser.js
@@ -0,0 +1,14 @@
+function setCurrentUser() {
+  var current_user = localStorage.getItem('current_user');
+  console.log("Current user: " + current_user);
+  if(current_user != null && JSON.parse(current_user) != null){
+    document.getElementById('logout_link').style.visibility = 'visible';
+    document.getElementById('favourites_link').style.visibility = 'visible';
+    document.getElementById('login_link').style.visibility = 'hidden';
+    document.getElementById('user_label').innerHTML = JSON.parse(current_user).user;
+  }
+}
+
+window.addEventListener('load', (event) => {
+  setCurrentUser();
+});
