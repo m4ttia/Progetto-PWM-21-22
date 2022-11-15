2b316a5ee39a67888bfbe59c64bcb929d44b1e78
m4ttia <mattia.garavaglia1@studenti.unimi.it>
2022-11-15 10:48:57 +0100

    Creazione script per il logout

---
+++ assets/script/scriptLogout.js
@@ -0,0 +1,11 @@
+function logout(){
+  localStorage.setItem('current_user',null);
+  document.getElementById('login_link').style.visibility = 'visible';
+  document.getElementById('logout_link').style.visibility = 'hidden';
+  document.getElementById('favourites_link').style.visibility = 'hidden';
+  document.getElementById('user_label').innerHTML = '';
+}
+
+document.getElementById('logout_link').addEventListener('click', (event) => {
+  logout();
+});
